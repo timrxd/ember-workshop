b976062204cd74544db30a9b37c45022fddea4b6
timrxd <timxdowd@gmail.com>
2017-12-04 09:37:48 -0500

    prettier cards

--- app/routes/deck.js
+++ app/routes/deck.js
@@ -4,10 +4,11 @@ export default Route.extend({
   model() {
     var deck = [];
     for (var i = 1; i <= 13; i++) {
-      deck.push('♡' + i);
-      deck.push('♠' + i);
-      deck.push('♣' + i);
-      deck.push('♢' + i);
+      var r = ["X","A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+      deck.push('♡' + r[i]);
+      deck.push('♠' + r[i]);
+      deck.push('♣' + r[i]);
+      deck.push('♢' + r[i]);
     }
     return deck;
   }
