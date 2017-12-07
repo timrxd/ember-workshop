dca8f8aff0e9bcb01ca712c58fe3dee2ec3b3c60
timrxd <timxdowd@gmail.com>
2017-12-07 08:50:46 -0500

    Added navbar

--- app/routes/deck.js
+++ app/routes/deck.js
@@ -3,8 +3,8 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     var deck = [];
-    for (var i = 1; i <= 13; i++) {
-      var r = ["X","A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    var r = ["X","A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    for (var i = 1; i <= 13; i++) {      
       deck.push('♡' + r[i]);
       deck.push('♠' + r[i]);
       deck.push('♣' + r[i]);
