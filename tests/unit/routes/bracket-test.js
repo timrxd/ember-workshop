23a89e6214031ce91bb3c5badc6472377bc4d6a6
timrxd <timxdowd@gmail.com>
2018-01-21 17:56:31 -0500

    new header w reactive links

---
+++ tests/unit/routes/bracket-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:bracket', 'Unit | Route | bracket', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
